80f91f33c664d0423e18ed573cae0302150e20fc
Catherine -marin <katerin243@hotmail.com>
2023-05-10 22:32:38 +0200

    primer commit

---
+++ src/navigator/Navigator.tsx
@@ -0,0 +1,22 @@
+
+import { createStackNavigator } from '@react-navigation/stack';
+import React from 'react';
+import { LoginScreen } from '../screens/LoginScreen';
+import { RegisterScreen } from '../screens/RegisterScreen';
+import { ProtectedScreen } from '../screens/ProtectedScreen';
+
+const Stack = createStackNavigator();
+export const  Navigator = () => {
+  return (
+    <Stack.Navigator screenOptions={{
+        headerShows:false,
+        cardStyle: {
+            backgroudColor:'white'
+        }
+    }}>
+      <Stack.Screen name="LoginScreen" component={LoginScreen} />
+      <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+      <Stack.Screen name="ProtectedScreen" component={ProtectedScreen} />
+    </Stack.Navigator>
+  );
+}
